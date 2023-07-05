938895260fefbce4a9a843d5d839c4dfec2ea6d4
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-05 19:21:55 +0500

    dbset post

--- Blog.Api/Context/AppDbContext.cs
+++ Blog.Api/Context/AppDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Like> Likes { get; set; }
+    public DbSet<Post> Posts { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
